d07b52aa552aab57fa548fe03f79edc88eee26ee
Kory Attleson <kory.attleson@gmail.com>
2021-11-04 09:41:41 -0500

    Created Cryptocurrencies, CryptoDetails, Exchanges, Homepage, and News.jsx files.

--- currency/src/components/index.js
+++ currency/src/components/index.js
@@ -1,1 +1,6 @@
 export { default as NavBar } from './NavBar';
+export { default as Cryptocurrencies } from './CryptoCurrencies'
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Exchanges } from './Exchanges';
+export { default as Homepage } from './Homepage';
+export { default as News } from './News';
